d280e07ae3831fa6e9409c8c19ab2e1d097bdcd8
faykarr <nasyathfaykar@gmail.com>
2024-06-15 10:33:33 +0700

    feat: add firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutterify/pages/splash_screen.dart';
 
-void main() {
+// Firebase import
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
